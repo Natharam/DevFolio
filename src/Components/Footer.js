ab95368fda6dfee447ab9bc257afab00a8775257
Natharam <201651033@iiitvadodara.ac.in>
2020-11-10 14:31:25 +0530

    commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,9 +7,7 @@ function Footer() {
   return (
     <div className="footer-container">
       <section className="footer-subscription">
-        <p className="footer-subscription-text">
-          Subscribe to get in touch.
-        </p>
+        <p className="footer-subscription-text">Subscribe to get in touch.</p>
         <div className="input-areas">
           <form>
             <input
@@ -26,18 +24,44 @@ function Footer() {
         <div className="footer-link-wrapper">
           <div className="footer-link-items">
             <h2>Contact Me</h2>
-
             <Link to="/contact">Contact</Link>
             <Link to="/about">Resume</Link>
+            <a href="mailto:contact@natharam.in" target="_blank">
+              Need any help?
+            </a>
           </div>
         </div>
         <div className="footer-link-wrapper">
           <div className="footer-link-items">
             <h2>Social Media</h2>
-            <a href="https://www.instagram.com/jaipal_natha_45_/" target="_blank" rel="noopener noreferrer">Instagram</a>
-            <a href="https://www.facebook.com/natharam.jaipal/" target="_blank" rel="noopener noreferrer">Facebook</a>
-            <a href="https://github.com/Natharam" target="_blank" rel="noopener noreferrer">Github</a>
-            <a href="https://www.linkedin.com/in/n-r-j-9b1abb131/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            <a
+              href="https://www.instagram.com/jaipal_natha_45_/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Instagram
+            </a>
+            <a
+              href="https://www.facebook.com/natharam.jaipal/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Facebook
+            </a>
+            <a
+              href="https://github.com/Natharam"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://www.linkedin.com/in/n-r-j-9b1abb131/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
           </div>
         </div>
       </div>
