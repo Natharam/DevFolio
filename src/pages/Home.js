2b38ac6278448ad9ff2c9104737691ceeb2a71cb
Natharam <201651033@iiitvadodara.ac.in>
2021-02-22 01:30:24 +0530

    profile changed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import '../App.css';
-import Body from '../Components/Body';
-import Footer from '../Components/Footer';
-
+import React from "react";
+import "../App.css";
+// import Body from '../Components/Body';
+import Footer from "../Components/Footer";
+const OtherComponent = React.lazy(() => import("../Components/Body"));
 function Home() {
   return (
     <>
-      <Body />
+      <React.Suspense fallback={<div style={{textAlign: "center"}}>Loading...</div>}>
+        <OtherComponent />
+      </React.Suspense>
       <Footer />
     </>
   );
