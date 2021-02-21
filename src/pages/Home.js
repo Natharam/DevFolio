0e3c68599bb6990cbb19982f004c31c7e89f9a6a
Natharam <201651033@iiitvadodara.ac.in>
2021-02-22 01:41:46 +0530

    profile changed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,17 @@ import "../App.css";
 import Footer from "../Components/Footer";
 const OtherComponent = React.lazy(() => import("../Components/Body"));
 function Home() {
+  const style = {
+    textAlign: "center", 
+    minHeight: "90vh", 
+    marginTop: "80", 
+    fontSize: "24", 
+    fontWeight: "600"
+  }
+  
   return (
     <>
-      <React.Suspense fallback={<div style={{textAlign: "center"}}>Loading...</div>}>
+      <React.Suspense fallback={<div style={style}>Loading...</div>}>
         <OtherComponent />
       </React.Suspense>
       <Footer />
